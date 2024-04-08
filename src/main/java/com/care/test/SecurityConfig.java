c3323e427b6752ac22432a97d7cf7b6260c76424
park <qkrwjdgus1441@naver.com>
2024-04-08 17:33:17 +0900

    영상 불러오기, 재생 완료

--- src/main/java/com/care/test/SecurityConfig.java
+++ src/main/java/com/care/test/SecurityConfig.java
@@ -3,11 +3,23 @@ package com.care.test;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 모든 도메인을 허용하거나, 필요에 따라 특정 도메인을 설정합니다.
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
+                .allowedHeaders("*"); // 허용할 헤더 설정
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
+
--- src/main/java/com/care/test/movie/MovieController.java
+++ src/main/java/com/care/test/movie/MovieController.java
@@ -2,17 +2,24 @@ package com.care.test.movie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    @GetMapping("/movie_list")
+
+    public String index(){
+        return "movie_list";
+    }
+
 
     @GetMapping("/upload")
     public String uploadMovie(){
